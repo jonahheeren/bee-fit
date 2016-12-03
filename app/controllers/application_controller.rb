32ff3a0096b8c22460c92c9a4a0f3d56fb42fe4d
Jonah Heeren <jonah.heeren@me.com>
2016-12-03 16:27:16 -0500

    Fixed Layout issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to '/login' unless current_user
+    redirect_to '/home' unless current_user
   end
 
 end
